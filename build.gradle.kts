dc4d639fb034a350cdf1bccf2daf15cfa5139803
Tofpu <tofpuu@gmail.com>
2023-07-30 20:15:02 +0300

    Remove lombok as stale dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
-    implementation("org.projectlombok:lombok:1.18.26")
     implementation("org.yaml:snakeyaml:2.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
